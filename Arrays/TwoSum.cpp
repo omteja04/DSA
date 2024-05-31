077e87d633f6213f84f0de7b64accd5c1efb701a
omteja04 <22P31A0539@acet.ac.in>
2024-05-31 20:57:56 +0530

    Two Sum

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -46,11 +46,12 @@ string twoSumOptimal(vector<int> &arr, int target) {
     int left = 0, right = arr.size() - 1;
     while (left < right) {
         int sum = arr[left] + arr[right];
-        if(sum ==target)
+        if (sum == target)
             return "YES";
-        else if(sum < target)
+        else if (sum < target)
             left++;
-        else right--;
+        else
+            right--;
     }
     return "NO";
 }
